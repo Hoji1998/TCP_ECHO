2bf3342a16df8287875e8a8d915903478951fc16
hoji1998 <bjh1157@naver.com>
2024-07-04 12:40:35 +0900

    🎉 add : constants와 utils생성 server와 client의 기본적인 패킷 교환

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 import net from 'net';
+import { readHeader, writeHeader } from './utils.js';
+import { HANDLER_ID, TOTAL_LENGTH_SIZE } from './constants.js';
 
 // 서버에 연결할 호스트와 포트
 const HOST = 'localhost';
@@ -8,10 +10,26 @@ const client = new net.Socket();
 
 client.connect(PORT, HOST, () => {
   console.log('Connected to server');
-})
+
+  const message = 'Hello';
+  const test = Buffer.from(message);
+
+  const header = writeHeader(test.length, 10);
+  const packet = Buffer.concat([header, test]);
+  client.write(packet);
+});
 
 client.on('data', (data) => {
-  console.log(data)
+  const buffer = Buffer.from(data);
+
+  const { handlerId, length } = readHeader(buffer);
+  console.log(`handlerId: ${handlerId}`);
+  console.log(`length: ${length}`);
+
+  const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
+  const message = buffer.slice(headerSize);
+
+  console.log(`server 에게 받은 메시지: ${message}`);
 });
 
 client.on('close', () => {
--- server.js
+++ server.js
@@ -1,24 +1,43 @@
 import net from 'net';
+import { readHeader, writeHeader } from './utils.js';
+import { HANDLER_ID, TOTAL_LENGTH_SIZE } from './constants.js';
 
 const PORT = 5555;
 
 const server = net.createServer((socket) => {
-  console.log(`Client connected: ${socket.remoteAddress}:${socket.remotePort}`);
+  console.log(`Client connected from: ${socket.remoteAddress}:${socket.remotePort}`);
 
   socket.on('data', (data) => {
-    console.log(data);
+
+    const buffer = Buffer.from(data);
+    const { handlerId, length } = readHeader(buffer);
+    console.log(`handlerId: ${handlerId}`);
+    console.log(`length: ${length}`);
+
+    const headerSize = TOTAL_LENGTH_SIZE + HANDLER_ID;
+    const message = buffer.slice(headerSize);
+
+    console.log(`client 에게 받은 메시지: ${message}`);
+
+    const responseMessage = "Hi!, There";
+    const responseBuffer = Buffer.from(responseMessage);
+
+    const header = writeHeader(responseBuffer.length, handlerId);
+    const packet = Buffer.concat([header, responseBuffer]);
+
+    socket.write(packet);
   });
 
   socket.on('end', () => {
-    console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
+    console.log('Client disconnected');
   });
 
   socket.on('error', (err) => {
-    console.log(`Socket error, ${err}`);
+    console.error('Socket error:', err);
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Echo Server listening on port ${PORT}`);
+  console.log(`Echo server listening on port ${PORT}`);
   console.log(server.address());
 });
