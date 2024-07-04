88197527284140fb974563eb6ea75138232669e5
hoji1998 <bjh1157@naver.com>
2024-07-04 09:54:43 +0900

    🎉 add : server와 client파일의 기본 세팅

---
+++ client.js
@@ -0,0 +1,23 @@
+import net from 'net';
+
+// 서버에 연결할 호스트와 포트
+const HOST = 'localhost';
+const PORT = 5555;
+
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+  console.log('Connected to server');
+})
+
+client.on('data', (data) => {
+  console.log(data)
+});
+
+client.on('close', () => {
+  console.log('Connection closed');
+});
+
+client.on('error', (err) => {
+  console.error('Client error:', err);
+});
